d617a5811fc84aaeed4af490560a4d95bb95f930
Sergio Enrico Coppolecchia <enricocoppolecchia@outlook.com>
2023-07-06 23:08:34 +0200

    setup Kdevelop projects

---
+++ Puntatori_esercitazioni/CardGame/build/CMakeFiles/CardGame.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CardGame.dir/main.c.o"
+  "CMakeFiles/CardGame.dir/main.c.o.d"
+  "CardGame"
+  "CardGame.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CardGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
